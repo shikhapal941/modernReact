4165788d4998981a90173a16905c24a5b6e5d1b6
Shikha pal <shikha.pal@smit.fit>
2024-08-22 19:04:46 +0530

    create own index.js file

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 1) import the react and reactdom libraries
+import React from "react";
+import ReactDOM from "react-dom/client";
+// 2) get a reference to the div with id root
+const el = document.getElementById('root')
+// 3) tell react to take control of that element
+const root = ReactDOM.createRoot(el)
+// 4) create a component
+function App() {
+  let message = "Hello My name is shikha"
+  if (Math.random() > 0.5)
+    {
+        message = "Hello there"
+    } 
+    return (
+      <h1>{new Date().toLocaleTimeString()}</h1>
+    )
+}
+// 5) show the component on the screen
+root.render(<App/>)
